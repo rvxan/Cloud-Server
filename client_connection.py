21c621ffb56760d4b45c7cb9e4f13bc67766673b
Wullff <89040132+Wullff@users.noreply.github.com>
2024-11-25 13:37:07 -0500

    Add files via upload

--- client_connection.py
+++ client_connection.py
@@ -1,53 +1,223 @@
-from client_functions import *
 
-#host = '127.0.0.1'
-#port = 3300
+import socket
+import struct
+import io
+import os
+
+host = '127.0.0.1'
+port = 3300
+
+BUFFER_SIZE = 1024
+
+RESPONSE_HEADER_SIZE = struct.calcsize('!il')
+
+#To Do:
+#Testing environment on laptop
+#Functions to: connect, Authenticate, Upload, Download, Delete, View Dir, Creade/Delete/Secure subfolder
+#Hide/prevent access to certain files and directories
+#Error handling
+#Multithreading
+
+#immediate todo:
+#before sending file, send preliminary info to server: what kind of operation to perform, location of file, maybe buffer size
+
+#initial message format:
+#signature "ntwrkprj"
+#integer representing type of request
+#integer representing size of next message
+#data to be processed by server
+
+#request types 0 = ping, 1 = send message to log, 2 = unused, 3 = upload file, 4 = download file, 5 = delete file/subfolder, 6 = view dir, 7 = change dir, 8 = create subfolder, 9 = delete subfolder
+
+#class to setup and organize data to be sent to the server in a single pass
+class ClientRequest:
+    def __init__(self):
+        self.ping_request()
+
+    #initializes a ping for this request
+    def ping_request(self):
+        self.type = 0
+
+    #intializes a message request for a string 'message'
+    def message_request(self, message):
+        self.type = 1
+        self.message = message
+
+    def upload_request(self, filename):
+        self.type = 2
+        self.filename = filename
+
+    #intializes a request to file at 'filepath' with string 'filename' local to the current directory at the server
+    def upload_accept(self, filepath, filename):
+        self.type = 3
+        self.filepath = filepath
+        self.filename = filename
+
+    #initializes a request to download a file object with filename string 'filepath'
+    def download_request(self, filepath):
+        self.type = 4
+        self.filepath = filepath
+
+    #initializes a delete request to delete a file object with filename string 'filepath'
+    def delete_request(self, filepath):
+        self.type = 5
+        self.filepath = filepath
+
+    #initalizes a dir request to obtain a string representation of the current directory at the server
+    def viewdir_request(self):
+        self.type = 6
+
+    #initializes a change directory request on the server using a string 'dirname'
+    def changedir_request(self, dirname):
+        self.type = 7
+        self.dirname = dirname
+
+    #initializes a create subfolder request on the server using a string 'dirname'
+    def createdir_request(self, dirname):
+        self.type = 8
+        self.dirname = dirname
+
+    #initializes a delete subfolder request on the server using a string 'dirname'
+    def deletedir_request(self, dirname):
+        self.type = 9
+        self.dirname = dirname
+
+    #the returned data of this function depends on self.type
+    #server side will use packed number to determine how to unpack data
+    def send_request(self, client_tcp):
+        #all requests always start with a signature and request type
+        #if signature is invalid, server won't send a return message
+
+        socket_send = SocketSend(client_tcp)
+
+        socket_send.send_bytes(struct.pack('!8si', b'ntwrkprj', self.type))
+        if self.type == 0:
+            pass
+        elif self.type == 1:
+            socket_send.send_string(self.message)
+        elif self.type == 2:
+            socket_send.send_string(self.filename)
+        elif self.type == 3:
+            socket_send.send_string(self.filename)
+            socket_send.send_file(self.filepath)
+        elif self.type == 4:
+            socket_send.send_string(self.filepath)
+        elif self.type == 5:
+            socket_send.send_string(self.filepath)
+        elif self.type == 6:
+            pass
+        elif self.type == 7:
+            socket_send.send_string(self.dirname)
+        elif self.type == 8:
+            socket_send.send_string(self.dirname)
+        elif self.type == 9:
+            socket_send.send_string(self.dirname)
+        elif self.type == 5232353:
+            print("HAHA")
+        socket_send.flush()
+
+#class to queue data sending
+class SocketSend:
+    def __init__(self, socket):
+        self.socket = socket
+        self.data_size = 0
+        self.data = b''
+    def send_string(self, message):
+        encodedMessage = message.encode('utf-8')
+        self.send_bytes(struct.pack('!i', len(encodedMessage)))
+        self.send_bytes(encodedMessage)
+    def send_file(self, filepath):
+        self.send_bytes(struct.pack('!l', os.stat(filepath).st_size))
+        with open(filepath, 'rb') as file:
+            while True:
+                file_data = file.read(BUFFER_SIZE)
+                if file_data == b'':
+                    break
+                self.send_bytes(file_data)
+    def send_bytes(self, new_data):
+        max_write_size = BUFFER_SIZE-self.data_size
+        if (len(new_data) > max_write_size):
+            self.data += new_data[0:max_write_size]
+            self.socket.send(self.data)
+            #reset data
+            self.data = new_data[max_write_size:len(new_data)]
+            self.data_size = len(new_data)-max_write_size
+        else:
+            self.data += new_data[0:max_write_size]  
+            self.data_size += len(new_data)
+    def flush(self):
+        self.socket.send(self.data)
 
 
-# To Do:
-# Testing environment on laptop
-# Functions to: connect, Authenticate, Upload, Download, Delete, View Dir, Creade/Delete/Secure subfolder
-# Hide/prevent access to certain files and directories
-# Error handling
-# Multithreading
+#returns (int,long) , where the int determines if server responded with a message string 0 or a file 1, and the first long indicates the size of total message
+def header_from_response(client_tcp):
+    return struct.unpack('!il', client_tcp.recv(BUFFER_SIZE, socket.MSG_PEEK)[0:RESPONSE_HEADER_SIZE])
 
-# immediate todo:
-# before sending file, send preliminary info to server: what kind of operation to perform, location of file, maybe buffer size
+#reads string data from the socket until socket is empty, returns a string
+def string_from_response(client_tcp):
+    dataHold = b''
+    data = client_tcp.recv(BUFFER_SIZE)
+    type_no, size = struct.unpack('!il', data[0:RESPONSE_HEADER_SIZE])
+    if type_no != 0:
+        raise Exception('this response doesn\'t contain a message')
 
-# initial message format:
-# signature "ntwrkprj"
-# integer representing type of request
-# integer representing size of next message
-# data to be processed by server
+    dataHold += data[RESPONSE_HEADER_SIZE:]
+    size -= BUFFER_SIZE-RESPONSE_HEADER_SIZE
+    while size > 0:
+        data = client_tcp.recv(BUFFER_SIZE)
+        dataHold += data
+        size -= BUFFER_SIZE
+    return dataHold.decode('utf-8')
+
+#reads bytes file data from the socket into the file path string 'filepath'
+def file_from_response(client_tcp, filepath):
+    with open(filepath, 'wb') as file:
+        data = client_tcp.recv(BUFFER_SIZE)
+        type_no, size = struct.unpack('!il', data[0:RESPONSE_HEADER_SIZE])
+
+        if type_no != 1:
+            raise Exception('this response doesn\'t contain a file')
+
+        file.write(data[RESPONSE_HEADER_SIZE:])
+        size -= BUFFER_SIZE-RESPONSE_HEADER_SIZE
+        while size > 0:
+            data = client_tcp.recv(BUFFER_SIZE)
+            file.write(data)
+            size -= BUFFER_SIZE
+
+#send data to the server using a provided stream and a socket
+def send_stream(stream, client_tcp):
+    while True:
+        data = stream.read(BUFFER_SIZE)
+        if data == b'':
+            break
+        client_tcp.send(data)
+
+       #data = client_tcp.recv(BUFFER_SIZE)
+       #print(f'The message recieved from the server: {data.decode("utf-8")}')
+
+def write_file_to_data(data, filepath): #data is passed as a copy?!?!?
+    with open(filepath, 'rb') as file:
+        while True:
+            file_data = file.read(BUFFER_SIZE)
+            print(data)
+            if file_data == b'':
+                break
+            data += file_data
+    return data
 
 
-# change this to generally initialize a connection with the ping request
-def setup_connection(host, port, username, password):
-    # TCP automatically handles resending packets+out of order packets. Robust packet management probs isn't necessary
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_tcp:
-        client_tcp.connect((host, port))
-        # client_tcp.send(message.encode('utf-8'))
-
-        # with open('TestingTextASCII.txt', 'rb') as file:
-        #    send_file(file, client_tcp)
-
-        request = ClientRequest()
-        request.ping_request()
-        # request.message_request('Testing Message!')
-        stream = io.BytesIO(request.to_byte_data())
-
-        if crashServer == True:
-            request.type = -1
-        print(request.to_byte_data())
-        request.send_request(client_tcp)
-
-        type_no, size, buffer_size = header_from_response(client_tcp)
-        message = string_from_response(client_tcp)
-        print(type_no)
-        print(message)
-
-    yield
-
+#send data to the server using a provided data and a socket
+def send_data(data, client_tcp):
+    send_stream(io.BytesIO(data), client_tcp)
 
 crashServer = False
+
+if __name__ == '__main__':
+    while True:
+        message = input('enter a message or q for quit: ')
+        if message == 'q':
+           quit()
+        if message == 'p':
+           crashServer = True
--- client_ui.py
+++ client_ui.py
@@ -1,10 +1,24 @@
+import os
+
 from client_connection import *
 import ipaddress
+import shlex
+
+client_tcp = None
+
+# change this to generally initialize a connection with the ping request
+def setup_connection(host, port, username, password):
+    # TCP automatically handles resending packets+out of order packets. Robust packet management probs isn't necessary
+    global client_tcp
+    client_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_tcp.connect((host, port))
+
+
 
 # Breaks input into requested number of values
 def parse_message(message, splits):
-    # Split the input string into parts by spaces
-    parts = message.strip().split(" ")
+    # Use shlex.split to split the input while preserving quoted substrings
+    parts = shlex.split(message.strip())
 
     # Pad the list with empty strings if fewer parts than num_parts
     parts += [""] * (splits - len(parts))
@@ -20,72 +34,112 @@ def is_valid_ip(ip):
     except ValueError:
         return False
 
-isConnected = 0
-
-while not isConnected:
-    message = input('Enter connect followed by an IP, port, username, and password to connect to a server, h for help, or q for quit: ')
-    command, host, port, username, password = parse_message(message, 5)
-    print(command, host, port, username, password)
-    if command.lower() == 'connect':
-        if is_valid_ip(host) and port.isdigit() and 1 <= int(port) <= 65535:
+while True:
+    while not client_tcp or client_tcp.fileno() == -1:
+        message = input('Enter connect followed by an IP, port, username, and password to connect to a server, h for help, or q for quit: ')
+        command, host, port, username, password = parse_message(message, 5)
+        if command.lower() == 'connect':
+            if port.isdigit():
+                port = int(port)
+                if is_valid_ip(host) and 1 <= port <= 65535:
+                    setup_connection(host, port, username, password)
+                else:
+                    print("Host or port is invalid.")
+            else:
+                print("Port should be an integer")
+        elif command == 'localhost':
+            setup_connection('127.0.0.1', 3300, username, password)
+        elif command == 'test':
             isConnected = 1
-            #setup_connection(host, port, username, password)
+        elif command == 'h':
+            print("connect [\033[3mServer IP\033[0m] [\033[3mPort\033[0m] - Connect to server at specified IP and port")
+        elif command == 'q':
+            quit()
         else:
-            print("Host or port is invalid.")
-    elif command == 'test':
-        isConnected = 1
-    elif command == 'h':
-        print("connect [\033[3mServer IP\033[0m] [\033[3mPort\033[0m] - Connect to server at specified IP and port")
-    elif command == 'q':
-        quit()
-    else:
-        print("Command not recognized.")
+            print("Command not recognized.")
 
-while isConnected:
-    request = ClientRequest()
-    message = input('Enter a command, h for help, or q to disconnect: ')
-    command, att1, att2 = parse_message(message, 3)
-    print(command, att1, att2)
-    if command.lower() == 'h':
-        print("upload [\033[3mfilepath\033[0m] [\033[3mfilename\033[0m] - Upload file located at \033[3mfilepath\033[0m to current server directory under the name \033[3mfilename\033[0m"
-              "\n                                   If a file name is not given, the file's existing name will be used."
-              "\ndownload [\033[3mfilepath\033[0m] - Download file located at \033[3mfilepath\033[0m from server"
-              "\ndelete [\033[3mfilepath\033[0m] - Delete file located at \033[3mfilepath\033[0m from server"
-              "\ndir - View files and subdirectories"
-              "\ncd [\033[3mdirectory\033[0m] - Change directory to [\033[3mdirectory\033[0m] in server")
-        continue
-    elif command.lower() == 'q':
-        print("isConnected = 0")
-        isConnected = 0
-        continue
-    elif command.lower() == 'upload':
-        if att1 == "":
-            print("When using the upload command, please specify a file to upload")
+    while client_tcp.fileno() != -1:
+        request = ClientRequest()
+        message = input('Enter a command, h for help, or q to disconnect: ')
+        command, att1, att2 = parse_message(message, 3)
+
+        if command.lower() == 'h':
+            print("upload [\033[3mfilepath\033[0m] - Upload file located at \033[3mfilepath\033[0m to current server directory"
+                  "\ndownload [\033[3mfilepath\033[0m] - Download file located at \033[3mfilepath\033[0m from server"
+                  "\ndelete [\033[3mfilepath\033[0m] - Delete file located at \033[3mfilepath\033[0m from server"
+                  "\ndir - View files and subdirectories"
+                  "\ncd [\033[3mdirectory\033[0m] - Change directory to [\033[3mdirectory\033[0m] in server")
             continue
-        print("request.upload_request(att1, att2)")
-        #request.upload_request(att1, att2)
-    elif command.lower() == 'download':
-        if att1 == "":
-            print("When using the download command, please specify a file to download")
+
+        elif command.lower() == 'q':
+            client_tcp.close()
             continue
-        print("request.download_request(att1)")
-        #request.download_request(att1)
-    elif command.lower() == 'delete':
-        if att1 == "":
-            print("When using the delete command, please specify a file to delete")
+
+        elif command.lower() == 'upload':
+
+            if not os.path.isfile(att1):
+                print("File is invalid")
+                continue
+
+            request.upload_request(os.path.basename(att1))
+            request.send_request(client_tcp)
+
+            if(string_from_response(client_tcp)) == "true":
+                while True:
+                    message = input('A file of that name already exists in the current server directory'
+                                    '\nWould you like to overwrite this file <Y/N>: ')
+                    if message.upper() == "Y":
+                        request.upload_accept(os.open(att1, os.O_RDONLY), os.path.basename(att1))
+                        break
+                    elif message.upper() == "N":
+                        break
+                    else:
+                        print("Please enter Y or N")
+            else:
+                request.upload_accept(os.open(att1, os.O_RDONLY), os.path.basename(att1))
+        elif command.lower() == 'download':
+            if att1 == "":
+                print("When using the download command, please specify a file to download")
+                continue
+            request.download_request(att1)
+        elif command.lower() == 'delete':
+            if att1 == "":
+                print("When using the delete command, please specify a file to delete")
+                continue
+            request.delete_request(att1)
+        elif command.lower() == 'dir':
+            request.viewdir_request()
+        elif command.lower() == 'cd':
+            if att1 == "":
+                print("When using the change directory command, please specify a target directory")
+                continue
+            request.changedir_request(att1)
+
+        elif command.lower() == 'subfolder':
+            if att2 == "":
+                print("When using the subfolder command, please specify a subfolder name to modify")
+                break
+            if att1.lower() == 'create':
+                request.createdir_request(att2)
+            elif att1.lower() == 'delete':
+                request.deletedir_request(att2)
+            else:
+                print("When using the subfolder command, specify if you would like to create or delete a subfolder")
+                break
+
+        else:
+            print("Command not recognized.")
             continue
-        print("request.delete_request(att1)")
-        #request.delete_request(att1)
-    elif command.lower() == 'dir':
-        print("request.viewdir_request()")
-        #request.viewdir_request()
-    elif command.lower() == 'cd':
-        if att1 == "":
-            print("When using the change directory command, please specify a target directory")
-            continue
-        print("request.changedir_request(att1)")
-        #request.changedir_request(att1)
-    else:
-        print("Command not recognized.")
-        continue
-    print("Exit test")
+
+        request.send_request(client_tcp)
+        responseType = header_from_response(client_tcp)
+
+        if responseType[0] == 0:
+            print(string_from_response(client_tcp))
+        elif responseType[0] == 1:
+            # Receiving file, store in current directory as downloaded file name
+            if not os.path.exists("downloads"):
+                os.mkdir("downloads")
+            file_from_response(client_tcp, os.getcwd() + "\\downloads\\" + os.path.basename(att1))
+        else:
+            errorStr = string_from_response(client_tcp)
